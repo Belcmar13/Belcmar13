b9a518191ce8706eb44f6b103b3f7767bd5af305
Mark belcourt <mark.belcourt@hotmail.com>
2024-06-17 13:48:54 -0500

    new installations
    \

---
+++ node.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDjF604elxSDvNrCsbt8NR2MaxwsHg0yrs",
+    authDomain: "quantombot7.firebaseapp.com",
+    projectId: "quantombot7",
+    storageBucket: "quantombot7.appspot.com",
+    messagingSenderId: "816704891937",
+    appId: "1:816704891937:web:0e8bedb05e145e5ab80760"
+  }
